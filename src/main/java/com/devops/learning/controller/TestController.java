b80fd0326e4e1a9f4bf4942d3e786bac1a667f94
MuruganGV <gomathivigneshmurugan@gmail.com>
2025-07-23 15:47:31 +0530

    Initial commit

---
+++ src/main/java/com/devops/learning/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.devops.learning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping(value = "/getData")
+    public String getData() {
+        return "Response from controller";
+    }
+}
